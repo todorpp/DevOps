d850773f74c96b872406bb76ccaec86dd9f776c9
Todor Parushev <Todor.Parushev@ibm.com>
2025-10-28 15:49:05 +0200

    set up actions workflows

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -39,7 +39,7 @@ data "aws_iam_policy_document" "tf_backend" {
       "dynamodb:DeleteItem"
     ]
     resources = [
-         "arn:aws:dynamodb:*:*:table/${var.tf_state_lock_table}"
+      "arn:aws:dynamodb:*:*:table/${var.tf_state_lock_table}"
     ]
   }
 
@@ -56,3 +56,40 @@ resource "aws_iam_user_policy_attachment" "tf_backend" {
   policy_arn = aws_iam_policy.tf_backend.arn
 }
 
+#########################
+# Policy for ECR access #
+#########################
+
+data "aws_iam_policy_document" "ecr" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:CompleteLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage"
+    ]
+    resources = [
+      aws_ecr_repository.app.arn,
+      aws_ecr_repository.proxy.arn,
+    ]
+  }
+}
+
+resource "aws_iam_policy" "ecr" {
+  name        = "${aws_iam_user.cd.name}-ecr"
+  description = "Allow user to manage ECR resources"
+  policy      = data.aws_iam_policy_document.ecr.json
+}
+
+resource "aws_iam_user_policy_attachment" "ecr" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.ecr.arn
+}
